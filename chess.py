899378ffe45ae3aaf3c64fbd65ffe5abb0e72edc
MARLIOT Baptiste <baptiste.marliot@vinci-energies.net>
2022-05-11 15:53:38 +0200

    ajout du déplacement des pièces

--- chess.py
+++ chess.py
@@ -1,7 +1,25 @@
+from tkinter import E
+import numpy as np
 import pygame
 
-
 # initialisation board
+
+def emptyBoard():
+    emptyBoard =  [[0, 0, 0, 0, 0, 0, 0],
+            [0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0], 
+            
+            [0,0,0,0,0,0,0,0], 
+            [0,0,0,0,0,0,0,0], 
+            [0,0,0,0,0,0,0,0], 
+            [0,0,0,0,0,0,0,0]]
+    return emptyBoard
+
+def tradBoard(listeCases):
+    emptyBoard= emptyBoard()
+    return np.reshape(listeCases,8,8)
+
 def initBoard() :  
     board =[[-5,-2,-3,-9,-50,-3,-2,-5],
             [-1,-1,-1,-1,-1,-1,-1,-1],
@@ -15,8 +33,6 @@ def initBoard() :
     return board
 board = initBoard()
 
-def deplacement(piece, endroit):
-    
 
 def drawBoard(board):
     listeCases=[]
@@ -54,7 +70,7 @@ class Case:
         self.color =color
 
 ### load piece
-        pygame.draw.rect(ecran ,  (color),(x,y,dimensionCase,dimensionCase))
+        pygame.draw.rect(ecran ,  (color),(x,y,dimensionCase,dimensionCase)) #tuile color 
         txt= ""
         if (piece == 0 ): 
             return
@@ -91,6 +107,36 @@ class Case:
             if self.x <= x <= self.x+dimensionCase and self.y <= y <= self.y+dimensionCase: # if cursos in case , return the name of the case
                 print(self.nom)
 
+def deplacement(case1, case2):
+    
+    for i in range (len(listeCases)):
+            if listeCases[i].nom == case1 :
+                caseA=listeCases[i]
+            elif listeCases[i].nom == case2 :
+                caseB=listeCases[i]
+    #A8
+    caseC= caseA.piece
+    caseA.piece=0  
+
+    depart= None
+    for i in range (len(listeCases)):
+        if listeCases[i].nom== case1:
+            depart =i
+            break
+    departX = depart//8 
+    departy = depart%8
+    board[departX][departy] = 0
+    
+    #A6
+    arrive  = None
+    caseB.piece= caseA.piece
+    for i in range (len(listeCases)):
+        if listeCases[i].nom== case2:
+            arrive =i
+            break
+    arriveX = arrive//8 
+    arriveY = arrive%8
+    board[arriveX][arriveY] = caseC
 
 
 pygame.init()
@@ -106,26 +152,6 @@ number=8  #number's name  of piece (will begin to A8, B8 , C8, ... ,A1)
 yInitial,xInitiale =100,100  # where begins the piece's placement
 pygame.draw.rect(ecran,(100,100,100), (0,0,800,800)) # background
 
-
-# for i in range (8):
-#     x=xInitiale
-#     for j in range (8):
-#         if (i + j) %2 == 0 : couleur = (201,209,242) # color
-#         else : couleur = (89, 113, 212) # color
-
-#         nomCase= chr(letter)+str(number)## case name ( to get the name from A8 to H1)
-#         letter+=1 ##increase the letter for name 
-        
-#         case= Case(ecran,nomCase,x,y,couleur,board[i][j]) #creation of the case
-#         listeCases.append(case)
-        
-#         pygame.display.flip()  # actualise    
-#         x+=dimensionCase #increase of case dimension for placement
-#     y+=dimensionCase #increase y for placment 
-    
-#     letter=65 #piece letter
-#     number-=1  ## piece numbrt
-
 listeCases = drawBoard(board)
 while continuer:
     letter=65 # ASCII CODE for letter A, will be usefull for piece name
@@ -133,8 +159,12 @@ while continuer:
     x,y= xInitiale,yInitial
     
     for event in pygame.event.get():
-        if event.type == pygame.QUIT or event.type == pygame.KEYDOWN:
+        if event.type == pygame.QUIT:
             continuer = False
+        if event.type == pygame.KEYDOWN:
+            
+            deplacement("A1","D4")
+            drawBoard(listeCases)
         for case in listeCases : 
             case.activeCase(event)
 
